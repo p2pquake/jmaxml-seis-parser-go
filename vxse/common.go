12553ad5c877ab3bbb8614e9eff4eaf295bac6a1
typewriter <takuyaxp@gmail.com>
2021-02-20 14:15:11 +0900

    Parse time

--- vxse/common.go
+++ vxse/common.go
@@ -1,5 +1,25 @@
 package vxse
 
+import (
+	"encoding/xml"
+	"time"
+)
+
 // W3C XML Schema dateTime 型による日付時刻表記
 // FIXME: Golang time.Time 型への変換未対応
-type DateTime string
+type DateTime time.Time
+
+func (dateTime *DateTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var s string
+	if err := d.DecodeElement(&s, &start); err != nil {
+		return err
+	}
+
+	dt, _ := time.Parse(time.RFC3339, s)
+	*dateTime = DateTime(dt)
+	return nil
+}
+
+func (dateTime *DateTime) Time() time.Time {
+	return time.Time(*dateTime)
+}
