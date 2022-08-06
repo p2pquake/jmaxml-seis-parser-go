48124cf4629de189926f64c017a6cc6dbcb41db0
typewriter <takuyaxp@gmail.com>
2022-08-06 16:45:02 +0900

    Add tests

--- converter/vxse2epsp_test.go
+++ converter/vxse2epsp_test.go
@@ -17,6 +17,7 @@ import (
 func TestVXSESmoke(t *testing.T) {
 	testVXSEDirectorySmoke(t, "../examples")
 	testVXSEDirectorySmoke(t, "../data")
+	testVXSEDirectorySmokeEEW(t, "../examples")
 }
 
 func TestCompareToHTMLAnalyzer(t *testing.T) {
@@ -145,6 +146,43 @@ func testVXSEDirectorySmoke(t *testing.T, dir string) {
 				} else {
 					t.Error(err)
 				}
+			}
+		})
+	}
+}
+
+func testVXSEDirectorySmokeEEW(t *testing.T, dir string) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		if !strings.HasSuffix(file.Name(), ".xml") {
+			continue
+		}
+
+		data, err := ioutil.ReadFile(dir + "/" + file.Name())
+		if err != nil {
+			panic(err)
+		}
+
+		v := &jmaseis.Report{}
+		t.Run("Parsable", func(t *testing.T) {
+			err = xml.Unmarshal(data, &v)
+			if err != nil {
+				t.Errorf("%s parse error: %#v", file.Name(), err)
+			}
+		})
+
+		t.Run("Convertable", func(t *testing.T) {
+			_, err = Vxse2EpspEEW(*v)
+			if err != nil {
+				_, ok := err.(*NotSupportedError)
+				if ok {
+					return
+				}
+				t.Errorf("%s convert error: %#v", file.Name(), err)
 			}
 		})
 	}
